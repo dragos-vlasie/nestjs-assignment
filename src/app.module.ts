d3d8a8383d2f68d009eee370d0351929462c5c40
dragos-vlasie <37800900+dragos-vlasie@users.noreply.github.com>
2024-04-07 13:07:32 +0100

    install/setup pg and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { CoreModule } from './core/core.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/config';
 
 @Module({
-  imports: [CoreModule, CatsModule],
+  imports: [
+    CoreModule, 
+    CatsModule, 
+    TypeOrmModule.forRoot(
+      typeOrmConfig
+    )
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -12,4 +12,5 @@ export class CatsService {
   findAll(): Cat[] {
     return this.cats;
   }
+
 }
